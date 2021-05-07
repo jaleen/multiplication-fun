71a6536c19dd88a15205fcfa79a1ace01cf80e54
Jalal <jalal.din@gmail.com>
2021-05-07 10:17:28 +0100

    created initial structure of the application

---
+++ src/test/java/com/deensoft/mathfun/MathfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deensoft.mathfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathfunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
