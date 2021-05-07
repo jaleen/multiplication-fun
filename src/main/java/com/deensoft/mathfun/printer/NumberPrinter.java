86438b5009d44931cc117327212aa9696055de64
Jalal <jalal.din@gmail.com>
2021-05-07 12:10:42 +0100

    can print numbers as well

---
+++ src/main/java/com/deensoft/mathfun/printer/NumberPrinter.java
@@ -0,0 +1,12 @@
+package com.deensoft.mathfun.printer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NumberPrinter {
+    private static Logger LOGGER = LoggerFactory.getLogger(BuzzPrinter.class);
+
+    public void print(short num){
+        LOGGER.info(String.valueOf(num));
+    }
+}
---
+++ src/test/java/com/deensoft/mathfun/printer/NumPrinterTest.java
@@ -0,0 +1,43 @@
+package com.deensoft.mathfun.printer;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import com.deensoft.mathfun.util.MemoryAppender;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class NumPrinterTest {
+
+    private static final String LOGGER_NAME = "com.deensoft.mathfun";
+    private MemoryAppender memoryAppender;
+
+    @Before
+    public void setup() {
+        Logger logger = (Logger) LoggerFactory.getLogger(LOGGER_NAME);
+        memoryAppender = new MemoryAppender();
+        memoryAppender.setContext((LoggerContext) LoggerFactory.getILoggerFactory());
+        logger.setLevel(Level.DEBUG);
+        logger.addAppender(memoryAppender);
+        memoryAppender.start();
+
+    }
+    @Test
+    public void test() {
+        NumberPrinter numPrinter = new NumberPrinter();
+
+        short num = 1;
+        numPrinter.print(num);
+        assertThat(memoryAppender.search( String.valueOf(num), Level.INFO).size()).isEqualTo(1);
+    }
+
+    @After
+    public void cleanUp() {
+        memoryAppender.reset();
+        memoryAppender.stop();
+    }
+}
