86b91e506f575b5340b182f722097aab1a6329ac
Jalal <jalal.din@gmail.com>
2021-05-07 11:46:18 +0100

    should be able to print Fizz

---
+++ src/main/java/com/deensoft/mathfun/print/FizzPrinter.java
@@ -0,0 +1,13 @@
+package com.deensoft.mathfun.print;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FizzPrinter {
+    public static final String FIZZ = "Fizz";
+    private static Logger LOGGER = LoggerFactory.getLogger(FizzPrinter.class);
+
+    public void print(){
+        LOGGER.info(FIZZ);
+    }
+}
